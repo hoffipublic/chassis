f1e381dd1d1d8fd1a78f64aeb3f51a9466e4ee91
Dirk Hoffmann <hoffi.mail@gmail.com>
2023-09-15 17:08:11 +0200

    docs/create.sh temporaryly awk alters docs/Gemfile and docs/_config.yml

--- docs/create.sh
+++ docs/create.sh
@@ -5,4 +5,13 @@
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$SCRIPTDIR" || exit 1
 
+# shellcheck disable=SC2016
+awkScript='/^#(.*)# CREATE.SH *$/ { print substr($0, 2) } /^[^#](.*)# CREATE.SH *$/ { print "#"$0 } !/# CREATE.SH *$/ { print $0 }'
+
+gawk -i inplace "$awkScript" "$SCRIPTDIR/_config.yml"
+gawk -i inplace "$awkScript" "$SCRIPTDIR/Gemfile"
+
 bundle exec jekyll serve
+
+gawk -i inplace "$awkScript" "$SCRIPTDIR/_config.yml"
+gawk -i inplace "$awkScript" "$SCRIPTDIR/Gemfile"
